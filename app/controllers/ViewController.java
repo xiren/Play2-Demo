cf7be79fe54fd778e1647d8f957c0c5f1c8a8073
kwang3 <King.Wang@morningstar.com>
2015-09-11 18:43:18 +0800

    加入AngularJS

---
+++ app/controllers/ViewController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.*;
+
+public class ViewController extends Controller{
+
+    public Result index(){
+        return ok(index.render());
+    }
+}
